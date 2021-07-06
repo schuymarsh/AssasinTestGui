7878f50350524692b1fbb03fd93afc7d1e61be64
Schuyler Marshall <schuymarsh@gmail.com>
2021-07-06 16:42:03 -0500

    Initial Commit
    Nothing currently

---
+++ Assasin Test/Assasin_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assasin_TestApp.swift
+//  Assasin Test
+//
+//  Created by Schuyler Marshall on 7/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Assasin_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assasin Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Assasin Test
+//
+//  Created by Schuyler Marshall on 7/6/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
